a49c907f44ceb9afddf75689ac74a1a8eaad204b
sabbey-skedulo <sam.abbey@skedulo.com>
2023-04-17 22:12:14 +0100

    Get all crosswords endpoint

--- src/crossword_scraper.rs
+++ src/crossword_scraper.rs
@@ -1,17 +1,19 @@
 use super::models::GuardianCrossword;
 
+extern crate futures;
 extern crate serde;
+use futures::future;
+use scraper::Html;
 
 pub async fn scrape_crossword(series: &str, id: String) -> Result<GuardianCrossword, String> {
     let url = format!("https://www.theguardian.com/crosswords/{}/{}", series, id);
-    let response = reqwest::get(url)
-        .await
-        .map_err(|e| format!("Error retrieving url response: {}", e.to_string()))?
-        .text()
-        .await
-        .map_err(|_| "Error getting text from response".to_string())?;
-    let document = scraper::Html::parse_document(&response);
-    let selector = scraper::Selector::parse(".js-crossword").unwrap();
+    scrape_crossword_at(url).await
+}
+
+async fn scrape_crossword_at(url: String) -> Result<GuardianCrossword, String> {
+    let document = get_document(url).await?;
+    let selector = scraper::Selector::parse(".js-crossword")
+        .map_err(|e| format!("Invalid selector: {}", e.to_string()))?;
     let element = document
         .select(&selector)
         .last()
@@ -22,4 +24,33 @@ pub async fn scrape_crossword(series: &str, id: String) -> Result<GuardianCrossw
         .map_or(Err("No attribute found".to_string()), Ok)?;
     serde_json::from_str(&json)
         .map_err(|e| format!("Error parsing crossword data: {}", e.to_string()))
+}
+
+async fn get_document(url: String) -> Result<Html, String> {
+    let response = reqwest::get(url)
+        .await
+        .map_err(|e| format!("Error retrieving url response: {}", e.to_string()))?
+        .text()
+        .await
+        .map_err(|_| "Error getting text from response".to_string())?;
+    Ok(scraper::Html::parse_document(&response))
+}
+
+pub async fn scrape_recent_crosswords(series: &str) -> Result<Vec<GuardianCrossword>, String> {
+    let url = format!("https://www.theguardian.com/crosswords/series/{}", series);
+    let document = get_document(url).await?;
+    let selector = scraper::Selector::parse(".fc-item__container>a")
+        .map_err(|e| format!("Invalid selector: {}", e.to_string()))?;
+    future::try_join_all(
+        document
+            .select(&selector)
+            .map(|s| s.value().attr("href"))
+            .flatten()
+            .map(|s| s.to_string())
+            .filter(|url| {
+                url.starts_with(format!("https://www.theguardian.com/crosswords/{}", series).as_str())
+            })
+            .map(|url| scrape_crossword_at(url)),
+    )
+    .await
 }
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,32 @@ mod models;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_crossword_data))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(get_crossword_data)
+            .service(get_all_crossword_data)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
 
 #[get("/crossword/{id}")]
 async fn get_crossword_data(path: web::Path<(String,)>) -> impl Responder {
     let crossword_id = path.into_inner().0;
     let crossword_data = crossword_scraper::scrape_crossword("cryptic", crossword_id).await;
+    return match crossword_data {
+        Ok(message) => serde_json::to_string(&message).map_or(
+            HttpResponse::BadRequest().body("Couldn't parse crossword to a string"),
+            |x| HttpResponse::Ok().body(x),
+        ),
+        Err(error) => HttpResponse::NotFound().body(error.to_string()),
+    };
+}
+
+#[get("/crosswords")]
+async fn get_all_crossword_data() -> impl Responder {
+    let crossword_data = crossword_scraper::scrape_recent_crosswords("cryptic").await;
     return match crossword_data {
         Ok(message) => serde_json::to_string(&message).map_or(
             HttpResponse::BadRequest().body("Couldn't parse crossword to a string"),
